48df7074ee30dc9f284e08113e086f88ff5d3597
hiba <hibaabdelkarim2000@gmail.com>
2021-03-01 10:19:41 +0200

    removed app content

--- src/App.js
+++ src/App.js
@@ -1,30 +1,6 @@
 import 'antd/dist/antd.css';
-import Map from './Components/Map/Map';
 function App() {
-  const CITIES = [
-    {
-      id: 0,
-      name: 'Tripoli',
-      description:
-        'Tripoli is the largest city in northern Lebanon and the second-largest city in the country',
-      lat: 34.4333,
-      lng: 35.8333,
-      address_coordinates: [34.4333, 35.8333],
-    },
-  ];
-  return (
-    <div className="App">
-      <Map
-        long={36.01043}
-        lat={34.24815}
-        zoomNb={12}
-        infoType={0}
-        width="50"
-        height="50ch"
-        data={CITIES}
-      />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
