629446b3951623bda02e252d3a7d5a49e2c25b75
hiba <hibaabdelkarim2000@gmail.com>
2021-03-01 10:01:59 +0200

    feat : added the map component with all its properties

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import 'antd/dist/antd.css';
-import ButtonComponont from './Components/./Button/Button';
-
+import Map from './Components/Map/Map';
 function App() {
-  return <div className="App"></div>;
+  const CITIES = [
+    {
+      id: 0,
+      name: 'Tripoli',
+      description:
+        'Tripoli is the largest city in northern Lebanon and the second-largest city in the country',
+      lat: 34.4333,
+      lng: 35.8333,
+      address_coordinates: [34.4333, 35.8333],
+    },
+  ];
+  return (
+    <div className="App">
+      <Map
+        long={36.01043}
+        lat={34.24815}
+        zoomNb={12}
+        infoType={0}
+        width="50"
+        height="50ch"
+        data={CITIES}
+      />
+    </div>
+  );
 }
 
 export default App;
