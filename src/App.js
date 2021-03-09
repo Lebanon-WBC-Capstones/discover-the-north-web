57104d4c088b8784f47914e446782c611e815ffe
hiba <hibaabdelkarim2000@gmail.com>
2021-03-09 22:49:45 +0200

    app content changed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import 'antd/dist/antd.css';
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import FooterComponent from './Components/Footer/Footer';
 import Navbar from './Components/Navbar/Navbar';
+import HomePage from './Pages/HomePage/HomePage';
 import AboutUsPage from './Pages/AboutUs/AboutUs';
+import SingleCityPage from './Pages/SingleCityPage/SingleCityPage';
 import CitiesPage from './Pages/CitiesPage/CitiesPage';
 import cities from './MockData/cities.json';
 import ErrorPage from './Pages/ErrorPage/ErrorPage';
 import TourGuidePage from './Pages/TourGuide/TourGuidePage';
+import ActivitiesPage from './Pages/ActivitiesPage/ActivitiesPage';
+import SingleActivityPage from './Pages/SingleActivityPage/SingleActivityPage';
 
+import './App.css';
 function App() {
   return (
     <div className="App">
@@ -16,19 +22,25 @@ function App() {
           <Navbar />
           <Switch>
             <Route exact path="/">
-              {/* Home page */}
+              <HomePage />
             </Route>
             <Route exact path="/activities">
-              {/* Activities page */}
+              <ActivitiesPage />
+            </Route>
+            <Route exact path="/activities/:id">
+              <SingleActivityPage />
             </Route>
             <Route exact path="/cities">
               <CitiesPage data={cities} />
+            </Route>
+            <Route exact path="/cities/:id">
+              <SingleCityPage />
             </Route>
             <Route exact path="/tour-guide">
               <TourGuidePage />
             </Route>
             <Route exact path="/user-profile">
-              {/* UserProfile page */}
+              UserProfile page
             </Route>
             <Route exact path="/about-us">
               <AboutUsPage />
@@ -37,6 +49,7 @@ function App() {
               <ErrorPage />
             </Route>
           </Switch>
+          <FooterComponent />
         </Suspense>
       </Router>
     </div>
